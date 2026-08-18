827881ba22643b0e30c006bd4838f8e3a43e2feb
MemoriesEndX <gemerznetindo@gmail.com>
2026-08-18 21:09:14 +0700

    feat: add android screen capture protection

--- android/app/src/main/java/co/id/krakatauposco/owl/MainActivity.java
+++ android/app/src/main/java/co/id/krakatauposco/owl/MainActivity.java
@@ -1,5 +1,18 @@
 package co.id.krakatauposco.owl;
 
+import android.os.Bundle;
+import android.view.WindowManager;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Native Android Screenshot & Screen Capture Protection
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        );
+    }
+}
